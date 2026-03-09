e90a9e24288539d027397fcab9e578988e6b9f25
Viviane S <vivi.suelen.silva@gmail.com>
2026-03-09 00:54:46 +0000

    novas variaveis inseridas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,72 +5,96 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
-// Área para definição das variáveis para armazenar as propriedades das cidades
-int  populacao, populacao2, pturistico, pturistico2;
-float area, area2, pib, pib2;
-char uf[2], uf2[2],codigo[5], codigo2[5], cidade[20], cidade2[20];
-// Área para entrada de dados
-printf("=========CARTA 1=========\n");
-printf("Digite o Estado: \n");
-scanf("%s", &uf);
+    //carta 1 variaveis
+    char estado;
+    char codigo[5];
+    char cidade[20];
+    int populacao, pontosturisticos;
+    float area, pib, densidadepopu, pibpercapita;
 
-printf("Digite o código: \n");
-scanf("%s", &codigo);
+    //carta 2 variaveis
+    char estado2;
+    char codigo2[5];
+    char cidade2[20];
+    int populacao2, pontosturisticos2;
+    float area2, pib2, densidadepopu2, pibpercapita2;
 
-printf("Digite a cidade: \n");
-scanf("%s", &cidade);
+    // Área para entrada de dados
+    printf("============== CARTA 1 ==============\n");
+    printf("Digite o Estado (uma letra): ");
+    scanf(" %c", &estado);
+    getchar(); // Limpar o buffer
+    
+    printf("Digite o Codigo (Max 4 letras): ");
+    scanf("%4s", codigo);
+    
+    printf("Digite a Cidade (s/ espaco, Max 19 letras): ");
+    scanf("%19s", cidade);
+    
+    printf("Digite a Populacao: ");
+    scanf("%d", &populacao);
+    
+    printf("Digite a Area: ");
+    scanf("%f", &area);
 
-printf("Digite a população: \n");
-scanf("%d", &populacao);
+    printf("Digite o PIB: ");
+    scanf("%f", &pib);
 
-printf("Digite a área: \n");    
-scanf("%f", &area);
+    printf("Pontos Turisticos: ");
+    scanf("%d", &pontosturisticos);
 
-printf("Digite o PIB: \n");    
-scanf("%f", &pib);
+    //insira os dados da carta 2
+    printf("============== CARTA 2 ==============\n");
+    printf("Digite o Estado (uma letra): ");
+    scanf(" %c", &estado2);
+    getchar(); // Limpar o buffer
 
-printf("Digite o número de pontos turísticos: \n");
-scanf("%d", &pturistico);
+    printf("Digite o Codigo (Max 4 letras): ");
+    scanf("%4s", codigo2);
 
-//insira os dados da carta 2
-printf("=========CARTA 2=========\n");
-printf("Digite o Estado: \n");
-scanf("%s", &uf2);
-   
-printf("Digite o código: \n");
-scanf("%s", &codigo2);
+    printf("Digite a Cidade (s/ espaco, Max 19 letras): ");
+    scanf("%19s", cidade2);
 
-printf("Digite a cidade: \n");
-scanf("%s", &cidade2);
+    printf("Digite a Populacao: ");
+    scanf("%d", &populacao2);
 
-printf("Digite a população: \n");
-scanf("%d", &populacao2);
+    printf("Digite a Area: ");
+    scanf("%f", &area2);
 
-printf("Digite a área: \n");    
-scanf("%f", &area2);
+    printf("Digite o PIB: ");
+    scanf("%f", &pib2);
 
-printf("Digite o PIB: \n");    
-scanf("%f", &pib2);
+    printf("Pontos Turisticos: ");
+    scanf("%d", &pontosturisticos2);
 
-printf("Digite o número de pontos turísticos: \n");
-scanf("%d", &pturistico2);   
+    // Área para exibição dos dados da carta 1 na tela
+    printf("============== CARTA 1 ==============\n");
+    printf("------------- Carta 1 ------------\n");
+    printf("Estado: %c\n", estado);
+    printf("Codigo: %s\n", codigo);
+    printf("Cidade: %s\n", cidade);
+    printf("Populacao: %d\n", populacao);
+    printf("Area: %.2f\n", area);
+    printf("PIB: %.2f\n", pib);
+    printf("Pontos Turisticos: %d\n", pontosturisticos);
+    densidadepopu = populacao / area;
+    printf("Densidade Populacional: %.2f\n", densidadepopu);
+    pibpercapita = (pib * 1000000000.0) / populacao;
+    printf("PIB per Capita: %.2f\n", pibpercapita);
 
-// Área para exibição dos dados da cidade
-printf("======CARTA 1======\n");
-printf("Código: %s\n", codigo);
-printf("Estado: %s\n", uf);
-printf("Nome da Cidade: %s\n", cidade);
-printf("População: %d\n", populacao);
-printf("PIB: %f\n", pib);
-printf("Pontos Turísticos: %d\n", pturistico);
 
-printf("======CARTA 2======\n");
-printf("Código: %s\n", codigo2);
-printf("Estado: %s\n", uf2);
-printf("Nome da Cidade: %s\n", cidade2);
-printf("População: %d\n", populacao2);
-printf("PIB: %f\n", pib2);
-printf("Pontos Turísticos: %d\n", pturistico2);
+    printf("============== CARTA 2 ==============\n");
+    printf("Estado: %c\n", estado2);
+    printf("Codigo: %s\n", codigo2);
+    printf("Cidade: %s\n", cidade2);
+    printf("Populacao: %d\n", populacao2);
+    printf("Area: %.2f\n", area2);
+    printf("PIB: %.2f\n", pib2);
+    printf("Pontos Turisticos: %d\n", pontosturisticos2);
+    densidadepopu2 = populacao2 / area2;
+    printf("Densidade Populacional: %.2f\n", densidadepopu2);
+    pibpercapita2 = (pib2 * 1000000000.0) / populacao2;
+    printf("PIB per Capita: %.2f\n", pibpercapita2);
 
 return 0;
 } 
